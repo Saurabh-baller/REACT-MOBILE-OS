f98d6e337b8ce9f5a5b716b4cf8aa9e1fa37eb92
Saurabh-baller <102128315+Saurabh-baller@users.noreply.github.com>
2022-05-17 00:32:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Os} from "./components/Os.jsx";
+function App() {
+  return (
+    <div className="App">
+     <Os />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Os.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Os = () => {
+  return (
+    <div>
+        <div>
+        <h3>Mobile Operating System</h3>
+        <li>Android</li>
+        <li>Blackberry</li>
+        <li>iPhone</li>
+        <li>Window's Phone</li>
+        </div>
+     <div>
+           
+        <h3>Mobile Manufactures</h3>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        
+         </div> 
+    </div>
+  )
+}
